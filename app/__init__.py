f1ed38efae3d183574034d337d7f72383a9114ea
kabagemark <kabagemark@gmail.com>
2018-06-28 11:11:23 +0300

    login and registration created

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import DevConfig
+ 
 
 
 app = Flask(__name__)
@@ -7,3 +8,7 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 from app.main import views
+
+# registering the blueprint
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,20 @@
 from app import app
 from flask import render_template
+from .forms import RegistrationForm,LoginForm
 
- 
+
+app.config['SECRET_KEY'] = 'kabagemark'  
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title = "register", form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = "login" , form = form)    
